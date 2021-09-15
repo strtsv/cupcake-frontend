36a3131f3e33108bfac13251b7fda46886e20ef7
jsusilo <enr@bk.ru>
2021-09-16 00:18:20 +0500

    Breadcrumbs blog list

---
+++ src/components/BlogList/Breadcrumbs.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import i from "../assets/images/bg-blog.jpg";
+
+class Breadcrumbs extends React.Component {
+  render() {
+    return (
+      <section className="breadcrumbs-custom">
+        <div className="parallax-container" data-parallax-img={i}>
+          <div className="breadcrumbs-custom-body parallax-content context-dark">
+            <div className="container">
+              <h1 className="breadcrumbs-custom-title">Blog List</h1>
+            </div>
+          </div>
+        </div>
+        <div className="breadcrumbs-custom-footer">
+          <div className="container">
+            <ul className="breadcrumbs-custom-path">
+              <li>
+                <a href="index.html">Home</a>
+              </li>
+              <li>
+                <a href="grid-blog.html">Blog</a>
+              </li>
+              <li className="active">Blog List</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Breadcrumbs;
