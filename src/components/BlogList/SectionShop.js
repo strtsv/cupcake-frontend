1491463a86cbba8906a8ed220f87559522050eda
jsusilo <enr@bk.ru>
2021-09-17 00:40:54 +0500

    blog-list section-shop

---
+++ src/components/BlogList/SectionShop.js
@@ -0,0 +1,333 @@
+import React from "react";
+
+import i1 from "../../assets/images/post-7-800x394.jpg";
+import i2 from "../../assets/images/post-8-800x394.jpg";
+import i3 from "../../assets/images/post-9-800x394.jpg";
+import i4 from "../../assets/images/user-4-123x123.jpg";
+import i5 from "../../assets/images/post-mini-1-106x104.jpg";
+import i6 from "../../assets/images/post-mini-2-106x104.jpg";
+
+class SectionShop extends React.Component {
+  render() {
+    return (
+      <section className="section section-xl bg-default text-md-left">
+        <div className="container">
+          <div className="row row-50 row-md-60">
+            <div className="col-lg-8 col-xl-9">
+              <div className="inset-xl-right-70">
+                <div className="row row-50 row-md-60 row-lg-80 row-xl-100">
+                  <div className="col-12">
+                    {}
+                    <article className="post post-modern box-xxl">
+                      <div className="post-modern-panel">
+                        <div>
+                          <a className="post-modern-tag" href="#">
+                            News
+                          </a>
+                        </div>
+                        <div>
+                          <time
+                            className="post-modern-time"
+                            dateTime="2019-08-09"
+                          >
+                            August 9, 2019
+                          </time>
+                        </div>
+                      </div>
+                      <h3 className="post-modern-title">
+                        <a href="blog-post.html">
+                          25 Useful Tips for Baking Delicious Cakes
+                        </a>
+                      </h3>
+                      <a className="post-modern-figure" href="blog-post.html">
+                        <img src={i1} alt width={800} height={394} />
+                      </a>
+                      <p className="post-modern-text">
+                        Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                        sed diam nonumy eirmod tempor invidunt ut labore et
+                        dolore magna aliquyam erat, sed diam voluptua. At vero
+                        eos et accusam et justo duo dolores et ea rebum. Stet
+                        clita kasd gubergren, no sea takimata sanctus est Lorem
+                        ipsum dolor sit amet. Lorem ipsum dolor sit amet
+                      </p>
+                      <a className="post-modern-link" href="blog-post.html">
+                        Read more
+                      </a>
+                    </article>
+                  </div>
+                  <div className="col-12">
+                    {}
+                    <article className="post post-modern box-xxl">
+                      <div className="post-modern-panel">
+                        <div>
+                          <a className="post-modern-tag" href="#">
+                            News
+                          </a>
+                        </div>
+                        <div>
+                          <time
+                            className="post-modern-time"
+                            dateTime="2019-08-09"
+                          >
+                            August 9, 2019
+                          </time>
+                        </div>
+                      </div>
+                      <h3 className="post-modern-title">
+                        <a href="blog-post.html">
+                          The Best Way to Make a Delicious and Tasty Butter Cake
+                        </a>
+                      </h3>
+                      <a className="post-modern-figure" href="blog-post.html">
+                        <img src={i2} alt width={800} height={394} />
+                      </a>
+                      <p className="post-modern-text">
+                        Cur abaculus ire? Exsul secundus historia est. Hilotaes
+                        persuadere! Est teres exemplar, cesaris. Altus hydras
+                        ducunt ad repressor. Pol, animalis! Rationes sunt
+                        abactors de ferox ignigena. Nunquam imperium cannabis.
+                        Hercle, magister audax!, hibrida! Accentors studere,
+                        tanquam mirabilis tumultumque. Vae, barbatus detrius!
+                      </p>
+                      <a className="post-modern-link" href="blog-post.html">
+                        Read more
+                      </a>
+                    </article>
+                  </div>
+                  <div className="col-12">
+                    {}
+                    <article className="post post-modern box-xxl">
+                      <div className="post-modern-panel">
+                        <div>
+                          <a className="post-modern-tag" href="#">
+                            News
+                          </a>
+                        </div>
+                        <div>
+                          <time
+                            className="post-modern-time"
+                            dateTime="2019-08-09"
+                          >
+                            August 9, 2019
+                          </time>
+                        </div>
+                      </div>
+                      <h3 className="post-modern-title">
+                        <a href="blog-post.html">
+                          Top 5 Wedding Cake Trends in 2019 According to Bakers
+                        </a>
+                      </h3>
+                      <a className="post-modern-figure" href="blog-post.html">
+                        <img src={i3} alt width={800} height={394} />
+                      </a>
+                      <p className="post-modern-text">
+                        Capios persuadere in oenipons! Hercle, sensorem
+                        peritus!, bassus poeta! Solem de bi-color competition,
+                        gratia resistentia! Ubi est gratis silva? Nunquam
+                        transferre ignigena. Verpa noceres, tanquam festus
+                        zirbus. A falsis, barcas peritus calcaria. Humani
+                        generiss messis! Eheu, noster bubo! Cum musa credere,
+                        omnes lacteaes
+                      </p>
+                      <a className="post-modern-link" href="blog-post.html">
+                        Read more
+                      </a>
+                    </article>
+                  </div>
+                </div>
+                <div className="pagination-wrap">
+                  {}
+                  <nav aria-label="Page navigation">
+                    <ul className="pagination">
+                      <li className="page-item page-item-control disabled">
+                        <a className="page-link" href="#" aria-label="Previous">
+                          <span className="icon" aria-hidden="true" />
+                        </a>
+                      </li>
+                      <li className="page-item active">
+                        <span className="page-link">1</span>
+                      </li>
+                      <li className="page-item">
+                        <a className="page-link" href="#">
+                          2
+                        </a>
+                      </li>
+                      <li className="page-item">
+                        <a className="page-link" href="#">
+                          3
+                        </a>
+                      </li>
+                      <li className="page-item page-item-control">
+                        <a className="page-link" href="#" aria-label="Next">
+                          <span className="icon" aria-hidden="true" />
+                        </a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-4 col-xl-3">
+              <div className="aside row row-30 row-md-50 justify-content-md-between">
+                <div className="aside-item col-12">
+                  <div className="team-info box-sm">
+                    <a className="team-info-figure" href="#">
+                      <img src={i4} alt width={123} height={123} />
+                    </a>
+                    <h6 className="team-info-title">
+                      <a href="#">Rebecca Harison</a>
+                    </h6>
+                    <p className="team-info-text">
+                      Sed eleifend, lacus nec bibendum pulvinar
+                    </p>
+                  </div>
+                  {}
+                  <form
+                    className="rd-search form-search"
+                    action="search-results.html"
+                    method="GET"
+                  >
+                    <div className="form-wrap">
+                      <label className="form-label" htmlFor="search-form">
+                        Search blog...
+                      </label>
+                      <input
+                        className="form-input"
+                        id="search-form"
+                        type="text"
+                        name="s"
+                        autoComplete="off"
+                      />
+                      <button
+                        className="button-search fl-bigmug-line-search74"
+                        type="submit"
+                      />
+                    </div>
+                  </form>
+                </div>
+                <div className="aside-item col-sm-6 col-md-5 col-lg-12">
+                  <h6 className="aside-title">Categories</h6>
+                  <ul className="list-categories">
+                    <li>
+                      <a href="#">News</a>
+                      <span className="list-categories-number">(18)</span>
+                    </li>
+                    <li>
+                      <a href="#">Cakes</a>
+                      <span className="list-categories-number">(9)</span>
+                    </li>
+                    <li>
+                      <a href="#">Baking</a>
+                      <span className="list-categories-number">(5)</span>
+                    </li>
+                    <li>
+                      <a href="#">Tips</a>
+                      <span className="list-categories-number">(8)</span>
+                    </li>
+                  </ul>
+                </div>
+                <div className="aside-item col-sm-6 col-lg-12">
+                  <h6 className="aside-title">Latest Posts</h6>
+                  <div className="row row-20 row-lg-30 gutters-10">
+                    <div className="col-6 col-lg-12">
+                      {}
+                      <article className="post post-minimal">
+                        <div className="unit unit-spacing-sm flex-column flex-lg-row align-items-lg-center">
+                          <div className="unit-left">
+                            <a
+                              className="post-minimal-figure"
+                              href="blog-post.html"
+                            >
+                              <img src={i5} alt width={106} height={104} />
+                            </a>
+                          </div>
+                          <div className="unit-body">
+                            <p className="post-minimal-title">
+                              <a href="blog-post.html">
+                                The Best Way to Make a Butter Cake
+                              </a>
+                            </p>
+                            <div className="post-minimal-time">
+                              <time dateTime="2019-03-15">March 15, 2019</time>
+                            </div>
+                          </div>
+                        </div>
+                      </article>
+                    </div>
+                    <div className="col-6 col-lg-12">
+                      {}
+                      <article className="post post-minimal">
+                        <div className="unit unit-spacing-sm flex-column flex-lg-row align-items-lg-center">
+                          <div className="unit-left">
+                            <a
+                              className="post-minimal-figure"
+                              href="blog-post.html"
+                            >
+                              <img src={i6} alt width={106} height={104} />
+                            </a>
+                          </div>
+                          <div className="unit-body">
+                            <p className="post-minimal-title">
+                              <a href="blog-post.html">
+                                Top 5 Wedding Cake Trends in 2019
+                              </a>
+                            </p>
+                            <div className="post-minimal-time">
+                              <time dateTime="2019-03-15">March 15, 2019</time>
+                            </div>
+                          </div>
+                        </div>
+                      </article>
+                    </div>
+                  </div>
+                </div>
+                <div className="aside-item col-sm-6 col-md-5 col-lg-12">
+                  <h6 className="aside-title">Popular tags</h6>
+                  <div className="group-sm group-tags">
+                    <a className="link-tag" href="#">
+                      News
+                    </a>
+                    <a className="link-tag" href="#">
+                      Bakery
+                    </a>
+                    <a className="link-tag" href="#">
+                      Tips
+                    </a>
+                    <a className="link-tag" href="#">
+                      Cakes
+                    </a>
+                    <a className="link-tag" href="#">
+                      Trends
+                    </a>
+                    <a className="link-tag" href="#">
+                      Pastry
+                    </a>
+                  </div>
+                </div>
+                <div className="aside-item col-sm-6 col-lg-12">
+                  <h6 className="aside-title">Archives</h6>
+                  <ul className="list-marked list-archives d-inline-block d-md-block">
+                    <li>
+                      <a href="#">March 2019</a>
+                    </li>
+                    <li>
+                      <a href="#">February 2019</a>
+                    </li>
+                    <li>
+                      <a href="#">January 2019</a>
+                    </li>
+                    <li>
+                      <a href="#">December 2019</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default SectionShop;
